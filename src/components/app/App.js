607bb958b6b836c64d0f9aa63536f70d5cea46b9
Guilherme Csorgo <csorgodev@hotmail.com>
2020-10-05 23:38:52 -0300

    I swear, now it will be working

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -4,7 +4,7 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      <p>Webhook test, second try</p>
+      <p>Webhook test, third try</p>
     </div>
   );
 }
