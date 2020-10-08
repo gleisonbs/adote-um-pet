c992fd3ce067ecf86106f9c31d28093c141b1638
Gleison <gleisonbs@gmail.com>
2020-10-08 13:48:54 -0300

    Add about page with routing

--- src/components/Navigation/NavigationItems/NavigationItem/index.js
+++ src/components/Navigation/NavigationItems/NavigationItem/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import styles from './NavigationItem.module.css'
-import { BrowserRouter as Router, NavLink } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
 const navigationItem = props => (
-  <Router className={styles.NavigationItem}>
     <NavLink to={props.uri}>
       {props.children}
     </NavLink>
-  </Router>
   );
 
 export default navigationItem;
--- src/components/UI/Logo/index.js
+++ src/components/UI/Logo/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom'
 import styles from './Logo.module.css'
 
 const logo = () => (
-    <div className={styles.Logo}>
-        <div>Logo</div>
-    </div>
-  );
+  <div className={styles.Logo}>
+    <NavLink to="/">
+      LOGO
+    </NavLink>
+  </div>
+);
 
 export default logo;
