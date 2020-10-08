b92314c4f5355f79e24ab570ba9176560fb416ee
Gleison <gleisonbs@gmail.com>
2020-10-08 09:51:14 -0300

    Fix App import in the App test suite

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import App from '../components/App/App'
+import App from '../components/App'
 
 import { configure, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
