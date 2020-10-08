e65976b357159c9b8fdae9793f235dfbdf3782d0
Gleison <gleisonbs@gmail.com>
2020-10-08 14:12:23 -0300

    Fix unused variable error

--- src/components/Navigation/NavigationItems/NavigationItem/index.js
+++ src/components/Navigation/NavigationItems/NavigationItem/index.js
@@ -3,9 +3,11 @@ import styles from './NavigationItem.module.css'
 import { NavLink } from 'react-router-dom'
 
 const navigationItem = props => (
+  <div className={styles.navigationItem}>
     <NavLink to={props.uri}>
       {props.children}
     </NavLink>
-  );
+  </div>
+);
 
 export default navigationItem;
