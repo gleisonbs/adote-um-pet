b9ce4efdaf0a58085b4c49c1b788c75dab9ff4a0
Guilherme Csorgo <csorgodev@hotmail.com>
2020-11-04 16:37:40 -0300

    Finalized the visual structure. No router were created yet

--- src/components/Login/LoginModal/index.js
+++ src/components/Login/LoginModal/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import { Modal } from 'semantic-ui-react';
 import styles from './LoginModal.module.css'
+
+import fbLogo from '../../../images/fbLogo.png';
+import glLogo from '../../../images/glLogo.png';
 
 function LoginModal({ id = 'modal', onClose = () => {}}) {
 
@@ -14,10 +16,34 @@ function LoginModal({ id = 'modal', onClose = () => {}}) {
                 <button className={styles.CloseBtn} onClick={onClose} />
                 <div className={styles.Content}>
                     <h3>Hora de fazer a diferença!</h3>
-                    <button className={styles.Btn} onClick={onClose} />
-                    <button className={styles.Btn} onClick={onClose} />
-                    <button className={styles.Btn} onClick={onClose} />
-                    <button className={styles.Btn} onClick={onClose} />
+                    <div className={styles.SocialMediaContainer}>
+                        <div className={styles.SocialMediaDiv}>
+                            <button className={styles.FacebookButton}>
+                                <img src={fbLogo} className={styles.socialMediaIconFacebook}/>
+                                Entrar com o facebook
+                            </button>
+                        </div>
+                        <div className={styles.SocialMediaDiv}>
+                            <button className={styles.GoogleButton}>
+                                <img src={glLogo} className={styles.socialMediaIconGoogle}/>
+                                Entrar com o Google
+                            </button>
+                        </div>
+                    </div>
+                    <div className={styles.LoginSplitter}>
+                        <hr/>
+                        ou
+                        <hr/>
+                    </div>
+                    <div className={styles.FormContainer}>
+                        <form>
+                            <input placeholder="Email"/>
+                            <input placeholder="Senha"/>
+                            <button type="submit" value="Entrar">
+                                Entrar
+                            </button>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
